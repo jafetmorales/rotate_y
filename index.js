5e382d5a5c4667368f3f845b3d7d8cbc7bcd6af5
Jafet Morales <jafet.aaron.morales@gmail.com>
2019-06-24 21:50:06 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+AFRAME.registerComponent('zinga_rotate_y', {
+    schema: {
+    speed: {type: 'number', default: .005}
+  },
+  init: function () {
+    console.log('Component attached!');
+  },
+    tick: function () {
+this.el.object3D.rotation.y=this.el.object3D.rotation.y+this.data.speed;   }
+});
